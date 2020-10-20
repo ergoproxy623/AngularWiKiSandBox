a1d990954c1c3e6f7441f22660c8186bed1c3dcf
diomid <diomidtreumov@gmail.com>
2020-10-20 15:05:49 +0300

    fix directive

--- src/app/modules/structure/directives/style.directive.ts
+++ src/app/modules/structure/directives/style.directive.ts
@@ -1,24 +1,27 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
-  selector: '[appStyle]'
+    selector: '[appStyle]'
 })
 export class StyleDirective {
-  @Input() color: string;
-  @Input() text: string;
-  prevValue: string;
+    @Input() color: string;
+    @Input() text: string;
+    prevValue: string;
+    @HostBinding('style.color') eColor = null;
 
-  constructor(private el: ElementRef,
-              private renderer: Renderer2) {
-  }
+    constructor(private el: ElementRef,
+                private renderer: Renderer2) {
+    }
 
-    @HostListener('click', ['$event.target']) onClick = event => {
-        if (event) {
-            this.prevValue = event.textContent;
-             if (this.prevValue !== this.text) {
-                 this.renderer.setStyle(this.el.nativeElement, 'color', this.color);
-                 event.textContent = this.text;
-             }
+    @HostListener('mouseenter') onEnter() {
+        console.log(this.el.nativeElement);
+        this.eColor = this.color;
+    }
+
+    @HostListener('click', ['$event.target']) onClick() {
+        if (this.el) {
+            this.renderer.setStyle(this.el.nativeElement, 'color', this.color);
+            this.el.nativeElement.textContent = this.text;
         }
     }
 
