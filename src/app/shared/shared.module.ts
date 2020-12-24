e31d20ec44f7dcdd1c4b646c0cac19bbb87c362e
diomid <diomidtreumov@gmail.com>
2020-12-24 15:46:16 +0200

    code style

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {CardItemComponent} from './base-elements/card-item/card-item.component';
 
 
-
 @NgModule({
-  declarations: [
-      CardItemComponent
-  ],
-  imports: [
-    CommonModule
-  ],
+    declarations: [
+        CardItemComponent
+    ],
+    imports: [
+        CommonModule
+    ],
     exports: [
         CardItemComponent
     ],
