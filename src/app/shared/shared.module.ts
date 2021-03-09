7d805643dfa991b887a82074524b2489ed95882c
EgoProxy623 <ergoproxy623@gmail.com>
2021-03-09 10:26:32 +0200

    add timeout directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {CardItemComponent} from './base-elements/card-item/card-item.component';
-import {FilterPipe} from '../modules/structure/pipes/filter.pipe';
-import {ModalComponent} from './base-elements/modal/modal.component';
-import {ResolveDirective} from '../directive/resolve.directive';
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {CardItemComponent} from "./base-elements/card-item/card-item.component";
+import {FilterPipe} from "../modules/structure/pipes/filter.pipe";
+import {ModalComponent} from "./base-elements/modal/modal.component";
+import {ResolveDirective} from "../directive/resolve.directive";
 import {DisableCOntrolDirective} from "../directive/disable-control.directive";
+import {TimeoutDirective} from "../directive/timeout.directive";
 
+const Components = [
+    TimeoutDirective
+];
 
 @NgModule({
     declarations: [
@@ -14,6 +18,7 @@ import {DisableCOntrolDirective} from "../directive/disable-control.directive";
         ModalComponent,
         ResolveDirective,
         DisableCOntrolDirective,
+        ...Components
     ],
     imports: [
         CommonModule,
@@ -23,6 +28,7 @@ import {DisableCOntrolDirective} from "../directive/disable-control.directive";
         FilterPipe,
         ResolveDirective,
         DisableCOntrolDirective,
+        ...Components
     ],
 })
 export class SharedModule {
