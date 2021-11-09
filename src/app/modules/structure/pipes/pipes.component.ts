0d9f9f308ebbd89d4156598862fa5ff4e2b78f3b
ROX-9 <you@example.com>
2021-11-09 11:13:53 +0200

    angular version updated

--- src/app/modules/structure/pipes/pipes.component.ts
+++ src/app/modules/structure/pipes/pipes.component.ts
@@ -1,48 +1,47 @@
-import {GetUser} from '../../../store/actions/user.action';
-import {Component, ComponentFactoryResolver, OnInit, ViewChild} from '@angular/core';
-import {Observable} from 'rxjs';
-import {TestRequestService} from '../../../services/test-request.service';
-import {FireServiceService} from '../../../services/fire-service/fire-service.service';
-import * as momentTZ from 'moment-timezone';
-import {IAppState} from '../../../store/state/app.state';
-import {select, Store} from '@ngrx/store';
-import {ActivatedRoute, Router} from '@angular/router';
-import {selectSelectedUser, selectUserList} from '../../../store/selectors/user.selectors';
-import {GetUsers} from '../../../store/actions/user.action';
-import {IUserDto} from '../../../classDTO/userDto/userDto';
-import {IConfig} from '../../../classDTO/config/config,interface';
-import {selectedConfig} from '../../../store/selectors/config.selectors';
-import {GetConfig, SetConfig} from '../../../store/actions/config.action';
-import {selectSelectedId} from '../../../store/selectors/router-state.selectors';
-import {ModalComponent} from '../../../shared/base-elements/modal/modal.component';
-import {ResolveDirective} from '../../../directive/resolve.directive';
+import { GetUser, GetUsers } from "../../../store/actions/user.action";
+import { Component, ComponentFactoryResolver, OnInit, ViewChild } from "@angular/core";
+import { Observable } from "rxjs";
+import { TestRequestService } from "../../../services/test-request.service";
+import { FireServiceService } from "../../../services/fire-service/fire-service.service";
+import * as momentTZ from "moment-timezone";
+import { IAppState } from "../../../store/state/app.state";
+import { select, Store } from "@ngrx/store";
+import { ActivatedRoute, Router } from "@angular/router";
+import { selectSelectedUser, selectUserList } from "../../../store/selectors/user.selectors";
+import { IUserDto } from "../../../classDTO/userDto/userDto";
+import { IConfig } from "../../../classDTO/config/config,interface";
+import { selectedConfig } from "../../../store/selectors/config.selectors";
+import { GetConfig, SetConfig } from "../../../store/actions/config.action";
+import { selectSelectedId } from "../../../store/selectors/router-state.selectors";
+import { ModalComponent } from "../../../shared/base-elements/modal/modal.component";
+import { ResolveDirective } from "../../../directive/resolve.directive";
 
 @Component({
-    selector: 'app-pipes',
-    templateUrl: './pipes.component.html',
-    styleUrls: ['./pipes.component.scss']
+    selector: "app-pipes",
+    templateUrl: "./pipes.component.html",
+    styleUrls: ["./pipes.component.scss"]
 })
 export class PipesComponent implements OnInit {
     tests = [0.12, 1.234234, 2.324534, 545645.5675, 7.23423];
     date = new Date();
     obj = {
-        name: 'JSON',
+        name: "JSON",
         line: 2
     };
 
     users: any[] = [];
     p: Promise<string> = new Promise<string>(resolve => {
         setTimeout(() => {
-            resolve('Promise done');
+            resolve("Promise done");
         }, 3000);
     });
 
     o: Observable<string> = new Observable<string>(observer => {
         setTimeout(() => {
-            observer.next('Observable next for 5 sec.');
+            observer.next("Observable next for 5 sec.");
         }, 5000);
         setTimeout(() => {
-            observer.next('Observable next for 7 sec.');
+            observer.next("Observable next for 7 sec.");
             observer.complete();
         }, 7000);
     });
@@ -59,7 +58,7 @@ export class PipesComponent implements OnInit {
         private store: Store<IAppState>,
         private router: Router,
         private activeRoute: ActivatedRoute,
-        private resolver: ComponentFactoryResolver,
+        private resolver: ComponentFactoryResolver
     ) {
     }
 
@@ -67,7 +66,7 @@ export class PipesComponent implements OnInit {
         this.store.dispatch(new GetUsers());
         this.store.dispatch(new GetConfig());
         setTimeout(() => {
-            this.store.dispatch(new SetConfig({adminName: 'Arthur', premissions: ['edit']}));
+            this.store.dispatch(new SetConfig({ adminName: "Arthur", premissions: ["edit"] }));
         }, 5000);
 
         console.log(this.activeRoute.children);
@@ -93,7 +92,7 @@ export class PipesComponent implements OnInit {
         this.store.dispatch(new GetUser(1));
         const modal = this.resolver.resolveComponentFactory(ModalComponent);
         const component = this.resolveDir.containerRef.createComponent(modal);
-        component.instance.title = 'New title';
+        component.instance.title = "New title";
         component.instance.close.subscribe(() => {
             this.resolveDir.containerRef.clear();
         });
--- src/test.ts
+++ src/test.ts
@@ -1,35 +1,33 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing';
-import {getTestBed} from '@angular/core/testing';
-import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
-import {RouterModule} from '@angular/router';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {AngularFireModule} from '@angular/fire';
-import {environment} from './environments/environment';
-import {StoreModule} from '@ngrx/store';
-import {appReducers} from './app/store/reducers/deducers-map';
+import "zone.js/testing";
+import { getTestBed } from "@angular/core/testing";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+import { RouterModule } from "@angular/router";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { StoreModule } from "@ngrx/store";
+import { appReducers } from "./app/store/reducers/deducers-map";
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
+    context(path: string, deep?: boolean, filter?: RegExp): {
+        keys(): string[];
+        <T>(id: string): T;
+    };
 };
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context("./", true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 
 
 export const TestBadImportConfig = () => {
-    return [ RouterModule.forRoot([]),
-            HttpClientTestingModule ,
-            StoreModule.forRoot(appReducers)];
+    return [RouterModule.forRoot([]),
+        HttpClientTestingModule,
+        StoreModule.forRoot(appReducers)];
 };
